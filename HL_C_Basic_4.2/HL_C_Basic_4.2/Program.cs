7ae92e08f303ebc8c03807753623a396ab12a4f2
Yevgenii Vasylenko <yevgenii.vasyenko@gmail.com>
2026-01-01 18:08:21 +0200

    ДЗ 4.2. Калькулятор

---
+++ HL_C_Basic_4.2/HL_C_Basic_4.2/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Заняття 4
+ДЗ 4.2. Калькулятор
+Користувач вводить два цiлих числа та математичну дію: + - * або /
+Залежно від введеної математичної дії вивести результат в консоль.
+Необхідно використати конструкцію switch у цьому завданні.
+*/
+//set unicode UTF8
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+//first num
+Console.WriteLine("Введіть перше число");
+double x = double.Parse(Console.ReadLine());
+//second num
+Console.WriteLine("Ввеіть другечисло");
+double y = double.Parse(Console.ReadLine());
+//set action
+Console.WriteLine("Ввеіть дію (+, -, *, /)");
+string action = Console.ReadLine();
+double result;
+//do action
+switch (action)
+{
+    case "+":
+        result = x + y;
+        break;
+    case "-":
+        result = x - y;
+        break;
+    case "*":
+        result = x * y;
+        break;
+    case "/":
+        if (y != 0)
+        {
+            result = x / y;
+        }
+        else
+        {
+            Console.WriteLine("Ділення на нуль неможливе.");
+            return;
+        }
+        break;
+    default:
+        Console.WriteLine("Щосьне те");
+        return;
+}
+//show result
+Console.WriteLine($"Результат: {result}");
+Console.WriteLine(result / 0);
+
